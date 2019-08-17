9b211db878109165a2106ff6a299f21573d35a7f
cheng <chengdingliang@gmail.com>
2019-08-17 20:31:53 +0800

    添加skills动画效果

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-window.onscroll = function (event) {
-    if(window.scrollY > 50) {
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+//导航栏浮动
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 50) {
         topNavBar.classList.add('sticky')
-    }else{
+    } else {
         topNavBar.classList.remove('sticky')
     }
-}
+})
+//技能条滚动
+me.classList.remove("offset")
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 652) {
+        let progressBar = document.querySelectorAll('.progress')
+        for (let i = 0; i < progressBar.length; i++) {
+            progressBar[i].classList.remove('default')
+
+        }
+        
+    }
+})
+
+
 let liTags = document.getElementsByClassName('menuTigger')
 for (let i = 0; i < liTags.length; i++) {
     
@@ -20,18 +40,74 @@ for (let i = 0; i < liTags.length; i++) {
     }
 }
 let aTags = document.querySelectorAll('nav > ul > li > a')
-console.log(aTags);
-for(let i = 0; i < aTags.length; i++){
+for (let i = 0; i < aTags.length; i++) {
     aTags[i].onclick = function (event) {
-        event.preventDefault();
-        let a = event.currentTarget
-        console.log(a.href);
+        event.preventDefault()
+        let a = event.currentTarget;
+        let href = a.getAttribute('href');
+        let element = document.querySelector(href);
+        let top = element.offsetTop
+
+        let currentTop = window.scrollY
+        let targetTop = top - 60;
+        let coords = { y:currentTop};
+        let s = targetTop - currentTop;
+        s = Math.abs(s);
+        var t = s * 3;
         
-        let href = a.getAttribute('href')
-        let element = document.querySelector(href)
-        let rect = element.getBoundingClientRect()
-        window.scrollTo(0,top - 80)
+        if(t > 500){ t = 500};
+       
         
-        
+        var tween = new TWEEN.Tween(coords)
+            .to({ y:targetTop }, t)
+            .easing(TWEEN.Easing.Quadratic.InOut)
+            .onUpdate(function () {      
+                window.scrollTo(0,coords.y)       
+            })
+            .start(); 
     }
+    
 }
+
+protfolioALL.onclick = function () {   
+    protfolioSlider.className = 'bar state-1'
+}
+protfolioFramework.onclick = function () {
+    protfolioSlider.className = 'bar state-2'
+}
+protfolioVallina.onclick = function () {
+    protfolioSlider.className = 'bar state-3'
+}
+
+window.addEventListener('scroll', function () {
+
+
+    let specialTages = document.querySelectorAll('[data-x]')
+    let minIndex = 0;
+    for (let i = 1; i < specialTages.length; i++) {
+        if ((Math.abs(specialTages[i].offsetTop - window.scrollY) < (Math.abs(specialTages[minIndex].offsetTop - window.scrollY)))) {
+                  minIndex = i;
+        }
+    }
+    
+    
+    specialTages[minIndex].classList.remove('offset')
+    let id = specialTages[minIndex].id
+    let a = document.querySelector('a[href="#' + id + '"]')
+    let li = a.parentNode;
+    let brothersAndMe = li.parentNode.children
+    
+    for (let i = 0; i < brothersAndMe.length; i++) {
+        brothersAndMe[i].classList.remove('highlight')
+    }
+    li.classList.add('highlight')
+
+});
+
+function findClosestAndRemoveOffset() {
+    
+    
+    
+    
+}  
+
