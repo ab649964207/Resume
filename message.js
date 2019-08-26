11c59f24eb6620f9f43b820f1291f09c89a7c66a
cheng <649964207@qq.com>
2019-08-26 21:37:51 +0800

    's'

--- message.js
+++ message.js
@@ -61,16 +61,6 @@
         bindEvents: function () {
             console.log(this);
 
-<<<<<<< HEAD
-            this.form.addEventListener('submit',  (event) =>{
-                event.preventDefault()
-
-                this.saveMessage()
-            })
-            this.avatar_selector.addEventListener('click', (event) => {
-                this.choseAvatar()
-            })
-=======
             this.form.addEventListener('submit', function (event) {
                 event.preventDefault()
 
@@ -79,7 +69,6 @@
             this.avatar_selector.addEventListener('click', function (event) {
                 this.choseAvatar()
             }.bind(controller))
->>>>>>> 0fe996546cd7fdebfbdf01dad46270b578d917f6
         },
         saveMessage: function () {
             myForm = this.form;
